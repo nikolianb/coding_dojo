fdd61c8b64e04a90e871e1d1a775314a557c6051
Nikolian <nikolian@gmx.com>
2025-01-06 13:39:33 +0100

    push

---
+++ Javascript_MERN_Assignments/Predict_Destructuring/predict_destructuring.js
@@ -0,0 +1,9 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+//The output will be Tesla, Mercedes
+console.log(randomCar)
+console.log(otherRandomCar)
+
+
